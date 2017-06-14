703824d37bfbd4c87a07855f1b88af57a029c40c
Kun Chen <kchen@csie.org>
2017-06-14 20:56:45 +0800

    change port

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.port || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
 
 
 // 路由
-app.get("/", function(req, res) {
+app.get("/test", function(req, res) {
     res.send("Hello");
 });
 
-app.listen(5438, function(req, res) {
-    console.log("Server init at port 5438");
+app.listen(port, function(req, res) {
+    console.log("Server init at port " + port);
 });
